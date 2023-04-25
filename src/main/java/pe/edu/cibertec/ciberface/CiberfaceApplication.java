ea6c71f39cd5c27235ad585aa8b71cc01b6c6ef2
quispewilmer <wilmerquispecode@gmail.com>
2023-04-24 23:31:54 -0500

    Add very basic project

---
+++ src/main/java/pe/edu/cibertec/ciberface/CiberfaceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ciberface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiberfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiberfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/ciberface/CiberfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ciberface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiberfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
