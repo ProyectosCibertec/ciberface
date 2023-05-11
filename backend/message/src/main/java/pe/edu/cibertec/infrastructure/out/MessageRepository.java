7d6d271d0ff163c4b9224b780d19f2be5d1e41c6
quispewilmer <wilmerquispecode@gmail.com>
2023-05-11 01:07:44 -0500

    Correct getMessagesByChat service

--- backend/message/src/main/java/pe/edu/cibertec/infrastructure/out/MessageRepository.java
+++ backend/message/src/main/java/pe/edu/cibertec/infrastructure/out/MessageRepository.java
@@ -1,6 +1,8 @@
 package pe.edu.cibertec.infrastructure.out;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.cibertec.domain.entity.Message;
 
@@ -8,5 +10,6 @@ import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByChatId(Long chatId);
+    @Query(value = "SELECT m FROM Message m WHERE m.chatId.chatId = :chatId")
+    List<Message> findByChatId(@Param("chatId") Long chatId);
 }
